57f86b518ca3050bdc8189ec9905662477e32549
Avneesh Paliya <avneeshcse@gmail.com>
2024-05-08 12:02:41 +0530

    changes by dev@av for issue fix on 08-may-2024

--- src/Components/Elements/StackModal.js
+++ src/Components/Elements/StackModal.js
@@ -99,17 +99,6 @@ const StackModal = (intialData) => {
         
     }, []);
 
-    useEffect(() => {
-        if(chatScroller){
-            // Get a reference to the div you want to auto-scroll.
-            const element = chatScroller.current;
-            // Create an observer and pass it a callback.
-            const observer = new MutationObserver(scrollToBottom);
-            // Tell it to look for new children that will change the height.
-            const config = {childList: true};
-            observer.observe(element, config);
-        }
-    }, [chatScroller])
 
     const scrollToBottom = (el) => {
         el.scrollTop = el.scrollHeight;
